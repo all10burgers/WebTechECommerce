c77cb15f9e9030c30c2f2a87569c03c2ea10c682
Phillipmoore01 <phillipmoore3102@gmail.com>
2023-11-30 15:38:55 -0800

    changing file structure

---
+++ WebTechECommerce/shop.php
@@ -0,0 +1,25 @@
+<?php 
+session_start(); 
+
+// Start the session 
+// Check if the add to cart button is clicked 
+if (isset($_POST["add_to_cart"])) { 
+	
+	// Get the product ID from the form 
+	$product_id = $_POST["product_id"]; 
+	
+	// Get the product quantity from the form 
+	$product_quantity = $_POST["product_quantity"]; 
+
+	// Initialize the cart session variable 
+	// if it does not exist 
+	if (!isset($_SESSION["cart"])) { 
+		$_SESSION["cart"] = []; 
+		header("location:cart.php"); 
+	} 
+
+	// Add the product and quantity to the cart 
+	$_SESSION["cart"][$product_id] = $product_quantity; 
+	header("location:cart.php"); 
+} 
+?> 
