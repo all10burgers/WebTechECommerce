35592844250e25c9758991d3ec39e6eefc70dbf9
all10burgers <travisalten@gmail.com>
2023-11-30 19:14:50 -0600

    fix login/register

--- WebTechECommerce/register.php
+++ WebTechECommerce/register.php
@@ -7,34 +7,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"]; 
     $password = $_POST["password"]; 
     
-    // Hash the password 
+   
     $hashed_password = password_hash($password, PASSWORD_BCRYPT); 
     $host = "webtechecommerce.mysql.database.azure.com"; 
     $dbname = "new_schema"; 
     $username_db = "rootuser"; 
     $password_db = "default123!"; 
-    try { 
-        $db = new PDO( 
-          "mysql:host=$host;dbname=$dbname",  
-              $username_db, $password_db); 
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        
-        // Insert the user into the database 
-        $stmt = $db->prepare( 
-          "INSERT INTO users (name,username,email, password)  
-            VALUES (:name, :username, :email,:password)"); 
-        $stmt->bindParam(":name", $name); 
-        $stmt->bindParam(":username", $username); 
-        $stmt->bindParam(":email", $email); 
-        $stmt->bindParam(":password", $hashed_password); 
-        $stmt->execute(); 
-        echo "<h2>Registration Successful</h2>"; 
-        echo "Thank you for registering, " . $name . "!<br>"; 
-        echo "You'll be redirected to login page in 3 seconds"; 
-        header("refresh:3;url=login.html"); 
-    } 
-    catch(PDOException $e) { 
-        echo "Connection failed: " . $e->getMessage(); 
-    } 
+    
+    $mysqli = new mysqli($host, $username_db, $password_db, $dbname);
+
+
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+
+    $stmt = $mysqli->prepare(
+        "INSERT INTO users (name, username, email, password)  
+        VALUES (?, ?, ?, ?)"
+    );
+    $stmt->bind_param("ssss", $name, $username, $email, $hashed_password);
+
+
+    $stmt->execute();
+
+    $stmt->close();
+    $mysqli->close();
+    echo "<h2>Registration Successful</h2>"; 
+    echo "Thank you for registering, " . $name . "!<br>"; 
+    echo "You'll be redirected to login page in 3 seconds"; 
+    header("refresh:3;url=login.html"); 
+   
 } 
 ?>
