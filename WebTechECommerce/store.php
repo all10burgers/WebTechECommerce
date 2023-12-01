496a2047149dfe3edee6b03848b9867b1e7d2d00
wcantu <wcantu5567@gmail.com>
2023-11-30 21:50:49 -0500

    store.php changes

--- WebTechECommerce/store.php
+++ WebTechECommerce/store.php
@@ -1,146 +1,65 @@
-<?php 
-session_start(); 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Group 5 Shopping Web Application</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="store.css">
+</head>
+<body>
+    <header class="bg-primary text-white text-center p-3">
+        <h1>Welcome <?php echo $_SESSION["user"]["name"]; ?> to Shopping Web Application</h1>
+    </header>
 
-// Start the session 
-// Check if the add to cart button is clicked 
-if (isset($_POST["add_to_cart"])) { 
-	
-	// Get the product ID from the form 
-	$product_id = $_POST["product_id"]; 
-	
-	// Get the product quantity from the form 
-	$product_quantity = $_POST["product_quantity"]; 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"><a class="nav-link" href="contact.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="cart.php">Cart</a></li>
+                <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </nav>
 
-	// Initialize the cart session variable 
-	// if it does not exist 
-	if (!isset($_SESSION["cart"])) { 
-		$_SESSION["cart"] = []; 
-		header("location:cart.php"); 
-	} 
+    <main class="container mt-4">
+        <section>
+            <h2 class="text-center mb-4">Products</h2>
+            <div class="row">
+                <!-- Product 1 -->
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" src="ProjPictures/adam-birkett-1M0omkZlGM4-unsplash.jpg" alt="Phone Charger">
+                        <div class="card-body">
+                            <h5 class="card-title">Phone Charger</h5>
+                            <p class="card-text">Standard 3ft Phone Charger</p>
+                            <p class="card-text"><strong>$12</strong></p>
+                            <form method="post" action="store.php" class="d-flex justify-content-between align-items-center">
+                                <div>
+                                    <input type="hidden" name="product_id" value="1">
+                                    <input type="number" id="product1_quantity" name="product_quantity" value="" min="0" max="10">
+                                </div>
+                                <button type="submit" name="add_to_cart" class="btn btn-primary">Add to Cart</button>
+                            </form>
+                            <a href="product_review.php?product_id=1" class="btn btn-secondary mt-2">View Reviews</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Product 2 -->
+                <!-- ... (Repeat the structure above for each product) -->
+            </div>
+        </section>
+    </main>
 
-	// Add the product and quantity to the cart 
-	$_SESSION["cart"][$product_id] = $product_quantity; 
-	header("location:cart.php"); 
-} 
-?> 
-<!DOCTYPE html> 
-<html> 
-	<head> 
-		<title>Group 5 Shopping Web Application</title> 
-		<link rel="stylesheet"
-				href="store.css"> 
-	</head> 
-	<body> 
-		<header> 
-			<h1>Welcome <?php 
-			$user = $_SESSION["user"]; 
-			echo $user["name"]; 
-			?> to Shopping Web Application</h1> 
-		</header> 
-		<nav> 
-			<ul> 
-				<li><a href="contact.php">Home</a></li> 
-				<li><a href="cart.php">Cart</a></li> 
-				<li><a href="logout.php">Logout</a></li> 
+    <footer class="bg-primary text-white text-center p-3">
+        <p>&copy; E-commerce site By Group 5</p>
+    </footer>
 
-			</ul> 
-		</nav> 
-		<main> 
-			<section> 
-				<h2>Products</h2> 
-				<ul> 
-					<li> 
-						<h3>Phone Charger</h3> 
-						<img src= "ProjPictures/adam-birkett-1M0omkZlGM4-unsplash.jpg"
-							alt="Product 1"> 
-						<p>Standard 3ft Phone Charger</p> 
-						<p><span>$12</span></p> 
-
-						<form method="post" action="store.php"> 
-							<input type="hidden"
-								name="product_id"
-								value="1"> 
-							<label for="product1_quantity"> 
-								Quantity: 
-							</label> 
-							<input type="number"
-								id="product1_quantity"
-								name="product_quantity"
-								value=""
-								min="0"
-								max="10"> 
-							<button type="submit"
-									name="add_to_cart"> 
-								Add to Cart</button> 
-						</form> 
-						<a href="product_review.php?product_id=1">View Reviews</a>
-					</li> 
-					<li> 
-						<h3>HeadPhones</h3> 
-						<img src= "ProjPictures/felipepelaquim-Zb6HZX6WHvU-unsplash.jpg"
-							alt="Product 2"> 
-						<p>Over Ear</p> 
-						<p> 
-							<span>$20</span> 
-						</p> 
-
-						<form method="post" action="store.php"> 
-							<input type="hidden"
-								name="product_id"
-								value="2"> 
-							<label for="product2_quantity"> 
-								Quantity: 
-							</label> 
-							<input type="number"
-								id="product2_quantity"
-								name="product_quantity"
-								value=""
-								min="0"
-								max="10"> 
-							<button type="submit"
-									name="add_to_cart"> 
-								Add to Cart 
-						</button> 
-						</form> 
-						<a href="product_review.php?product_id=2">View Reviews</a>
-					</li> 
-					<li> 
-						<h3>Phone Case</h3> 
-						<img src= "ProjPictures/jascent-leung-CubSQS4iYEE-unsplash.jpg"
-							alt="Product 3"> 
-						<p>Dark Themed Phone Case</p> 
-						<p> 
-							<span>$50</span> 
-						</p> 
-
-						<form method="post" action="sore.php"> 
-							<input type="hidden"
-								name="product_id"
-								value="3"> 
-							<label for="product3_quantity"> 
-								Quantity: 
-							</label> 
-							<input type="number"
-								id="product3_quantity"
-								name="product_quantity"
-								value=""
-								min="0"
-								max="10"> 
-							<button type="submit"
-									name="add_to_cart"> 
-								Add to Cart 
-							</button> 
-						</form> 
-						<a href="product_review.php?product_id=3">View Reviews</a>
-					</li> 
-								
-					<!-- Add forms for the other products here --> 
-				</ul> 
-			</section> 
-		</main> 
-		<footer> 
-			<p>&copy; E-commerce site By Group 5</p> 
-		</footer> 
-		<script src="store.php"></script> 
-	</body> 
+    <!-- Bootstrap JS and other necessary scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
 </html>
